f014f0667ed172f0628265ed86a9e6ee3752d316
tx <467220125@qq.com>
2017-10-25 23:17:48 +0800

    添加static文件

--- cake/views.py
+++ cake/views.py
@@ -5,9 +5,9 @@ from .models import Category, Cake1
 
 def category(request):
     categorys = Category.objects.all()
-    t = Category.objects.get(id=1)
-    q = Cake1.objects.filter(category_id=categorys[0].id)
+    # t = Category.objects.get(id=1)
+    # q = Cake1.objects.filter(category_id=categorys[0].id)
     # cake = categorys.cake1_set.all()
-    w = get_list_or_404(Cake1, category_id=categorys[0].id)
-    print(w)
+    # w = get_list_or_404(Cake1, category_id=categorys[0].id)
+    print(categorys)
     return render(request, "cake/cake.html", {"categorys": categorys})
