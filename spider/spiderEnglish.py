fb54b5a8aa81d3a4176bbe08699bf35e3f26a710
tx <467220125@qq.com>
2017-12-26 22:46:47 +0800

    爬虫

---
+++ spider/spiderEnglish.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+response = requests.get("http://m.chinavoa.com/51voa/")
+soup = BeautifulSoup(response.content, "html5lib")
+content = soup.find("dl", class_="block_con").dd.ul
+lilist = content.find_all("li")
+
+detailRep = requests.get(lilist[0].a.get("href"))
+detailSoup = BeautifulSoup(detailRep.content, "html5lib")
+plist = detailSoup.find("div", class_="neirong").find_all("p")
+for p in plist:
+    if p.string:
+        print(p.string)
+
+# for li in lilist:
+#     if "VOA慢速英语" in li.a.contents[1]:
+#         url2 = requests.get(li.a.get("href"))
+#         print(url2)
+#         print(li.a.contents[0].string)
+#         print(li.a.contents[1])
+#         print(li.a.get("href") + "\n")
