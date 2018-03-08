d169a5f5dd69411d93715f97a70eaf32000bcbbd
taixiang <taixiang@htyg.com.cn>
2018-03-08 18:30:40 +0800

    222

--- cake/serializer.py
+++ cake/serializer.py
@@ -10,24 +10,24 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ('id', 'category', 'pub_time', 'order')
 
 
-class CakeSerializer(serializers.ModelSerializer):
-    # sub = SecondeSerializer(many=True)
-
-    pub_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
-    category_id = serializers.StringRelatedField(many=True)
-
-    class Meta:
-        model = Cake1
-        fields = ('id', 'name', 'price', 'discount_price', 'desc', 'label', 'pub_time', 'category_id')
+# class FilterSerializer(serializers.ListSerializer):
+#     def to_representation(self, data):
+#         return super(FilterSerializer, self).to_representation(data)
 
 
 class SecondeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('category',)
-        list_serializer_class = FilterSerializer
+        # list_serializer_class = FilterSerializer
 
 
-class FilterSerializer(serializers.ListSerializer):
-    def to_representation(self, data):
-        return super(FilterSerializer, self).to_representation(data)
+class CakeSerializer(serializers.ModelSerializer):
+    category_id = SecondeSerializer(many=True)
+
+    pub_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+    # category_id = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Cake1
+        fields = ('id', 'name', 'price', 'discount_price', 'desc', 'label', 'pub_time', 'category_id')
