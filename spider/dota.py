8a04b2b772f3707463a19e2ee19ec078468e0692
taixiang <taixiang@htyg.com.cn>
2018-08-23 15:13:44 +0800

    柱状图

--- spider/dota.py
+++ spider/dota.py
@@ -6,7 +6,7 @@ import copy
 
 
 # item 指radiant 或dire 的bans、picks列表数据
-def bp(item, bp_dict, tag):
+def bp(item, bp_dict):
     # 遍历bans 或picks 数据
     for i, bp in enumerate(item):
         key = bp["name"]
@@ -14,7 +14,6 @@ def bp(item, bp_dict, tag):
         if key in bp_dict.keys():
             bp_dict[key]["count"] = bp_dict[key]["count"] + 1
         else:  # 不存在就记录一条数据
-            # bp.update(count=1)
             bp_dict[key] = copy.deepcopy(bp)
             bp_dict[key].update(count=1)
     return bp_dict
@@ -42,21 +41,19 @@ def dota():
                 print(new_b_list)
                 print(new_p_list)
                 print(new_bp_list)
-                # create_excel(new_b_list, new_p_list, new_bp_list)
+                create_excel(new_b_list, new_p_list, new_bp_list)
                 # print(p_dict)
                 return
-            bp(item["radiant"]["bans"], b_dict, "1111")
-            bp(item["dire"]["bans"], b_dict, "2222")
-            bp(item["radiant"]["picks"], p_dict, "3333")
-            bp(item["dire"]["picks"], p_dict, "44444")
+            bp(item["radiant"]["bans"], b_dict)
+            bp(item["dire"]["bans"], b_dict)
+            bp(item["radiant"]["picks"], p_dict)
+            bp(item["dire"]["picks"], p_dict)
 
+            bp(item["radiant"]["bans"], bp_dict)
+            bp(item["radiant"]["picks"], bp_dict)
+            bp(item["dire"]["bans"], bp_dict)
+            bp(item["dire"]["picks"], bp_dict)
 
-            bp(item["radiant"]["bans"], bp_dict, "5555")
-            bp(item["radiant"]["picks"], bp_dict, "66666")
-            bp(item["dire"]["bans"], bp_dict, "77777")
-            bp(item["dire"]["picks"], bp_dict, "8888")
-
-            print("-------------")
             if item["radiant_win"] == 0:
                 if item["dire"]["team_id"] == "726228":
                     bp(item["dire"]["bans"], b_win_dict)
@@ -65,72 +62,66 @@ def dota():
                 if item["radiant"]["team_id"] == "726228":
                     bp(item["radiant"]["bans"], b_win_dict)
                     bp(item["radiant"]["picks"], p_win_dict)
-        print(b_dict)
-        print(p_dict)
-        print(bp_dict)
-        return
+                    # print(b_dict)
+                    # print(p_dict)
+                    # print(bp_dict)
+                    # return
 
 
-def create_excel(b_list, p_dict, bp_list):
+def create_excel(b_list, p_list, bp_list):
     # 创建excel表格
     file = xlsxwriter.Workbook("dota.xlsx")
     # 创建工作表1
     sheet1 = file.add_worksheet("sheet1")
-    headers = ["", "英雄", "ban", "", "", "英雄", "pick", "", "", "英雄", "bp_all"]
-    # 写表头
+    # 创建表头
+    headers = ["图片", "英雄", "ban", "", "图片", "英雄", "pick", "", "图片", "英雄", "bp_all"]
     for i, header in enumerate(headers):
         # 第一行为表头
         sheet1.write(0, i, header)
 
-    # ban
-    for row in range(len(b_list)):  # 行
-        # 设置行高
-        sheet1.set_row(row + 1, 60)
-        for col in range(len(headers)):  # 列
-            if col == 0:
-                url = "http://cdn.dotamore.com/heros_id_62_35/%d.png" % b_list[row][1]["id"]
-                image_data = BytesIO(urlopen(url).read())
-                sheet1.insert_image(row + 1, col, url, {"image_data": image_data})
-            if col == 1:
-                name = b_list[row][1]["name_cn"]
-                sheet1.write(row + 1, col, name)
-            if col == 2:
-                count = b_list[row][1]["count"]
-                sheet1.write(row + 1, col, count)
+    insert_data(sheet1, headers, b_list, 0, 1, 2)
+    insert_data(sheet1, headers, p_list, 4, 5, 6)
+    insert_data(sheet1, headers, bp_list, 8, 9, 10)
 
-    # pick
-    for row in range(len(p_dict)):  # 行
-        # 设置行高
-        sheet1.set_row(row + 1, 60)
-        for col in range(len(headers)):  # 列
-            if col == 4:
-                url = "http://cdn.dotamore.com/heros_id_62_35/%d.png" % p_dict[row][1]["id"]
-                image_data = BytesIO(urlopen(url).read())
-                sheet1.insert_image(row + 1, col, url, {"image_data": image_data})
-            if col == 5:
-                name = p_dict[row][1]["name_cn"]
-                sheet1.write(row + 1, col, name)
-            if col == 6:
-                count = p_dict[row][1]["count"]
-                sheet1.write(row + 1, col, count)
+    insert_chart(file, sheet1, b_list, "b_list", "M2", 1, 2)
+    insert_chart(file, sheet1, p_list, "p_list", "M13", 5, 6)
+    insert_chart(file, sheet1, bp_list, "bp_list", "M24", 9, 10)
 
-    # bp_list
+    # 关闭表格
+    file.close()
+
+
+def insert_data(sheet1, headers, bp_list, col1, col2, col3):
     for row in range(len(bp_list)):  # 行
         # 设置行高
-        sheet1.set_row(row + 1, 60)
+        sheet1.set_row(row + 1, 30)
         for col in range(len(headers)):  # 列
-            if col == 8:
+            if col == col1:  # 英雄图片，根据id获取
                 url = "http://cdn.dotamore.com/heros_id_62_35/%d.png" % bp_list[row][1]["id"]
                 image_data = BytesIO(urlopen(url).read())
                 sheet1.insert_image(row + 1, col, url, {"image_data": image_data})
-            if col == 9:
+            if col == col2:  # 英雄名
                 name = bp_list[row][1]["name_cn"]
                 sheet1.write(row + 1, col, name)
-            if col == 10:
+            if col == col3:  # 统计次数
                 count = bp_list[row][1]["count"]
                 sheet1.write(row + 1, col, count)
-    # 关闭表格
-    file.close()
+
+
+# 插入柱状图
+def insert_chart(file, sheet1, bp_list, name, M, col_x, col_y):
+    chart = file.add_chart({"type": "column"})  # 柱状图
+    chart.add_series({
+        "categories": ["sheet1", 1, col_x, len(bp_list), col_x],  # 图表类别标签范围，x轴，这里取英雄的名字，即英雄名字那一列，行数根据数据列表确定
+        "values": ["sheet1", 1, col_y, len(bp_list), col_y],  # 图表数据范围，y轴，即次数那一列，行数根据数据列表确定
+        "data_labels": {"value": True},
+    })
+    chart.set_title({"name": name})  # 图表标题
+    chart.set_size({"width": 1000, "height": 400})
+    chart.set_x_axis({'name': '英雄'})  # x轴描述
+    chart.set_y_axis({'name': '次数'})  # y轴描述
+    chart.set_style(3)  # 直方图类型
+    sheet1.insert_chart(M, chart)  # 在表格M处插入柱状图
 
 
 dota()
