b1986219f9a1370fa6edab311c8cf8f46ad1eab8
taixiang <taixiang@htyg.com.cn>
2018-11-15 18:33:42 +0800

    word

--- work/word.py
+++ work/word.py
@@ -1,7 +1,24 @@
-import docx
+from pydocx import PyDocX
+import os
+from win32com import client
 
 def createDoc():
-    doc = docx.Document("1111.docx")
-    print(doc)
+    html = PyDocX.to_html("111.doc")
+    print(html)
+    with open("1.html", 'w') as f:
+        f.write(html)
 
-createDoc()
+
+def re_name(dir_path, old_file, new_file):
+    os.renames(dir_path + "/" + old_file, dir_path + "/" + new_file)
+
+
+def re_name1(old_file, new_file):
+    word = client.Dispatch("Word.Application")
+    doc = word.Documents.Open("D:\\mine\\py\\py_cake\\work\\111.doc")
+    doc.SaveAs("D:\\mine\\py\\py_cake\\work\\6666.docx")
+    doc.Close()
+    word.Quit()
+
+re_name1("111.doc", "111.docx")
+# createDoc()
