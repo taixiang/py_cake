8c8513b4f1d93b886893ae6ff19ea29fb7a8973f
taixiang <taixiang@htyg.com.cn>
2019-01-10 15:11:15 +0800

    tj

--- work/blog.py
+++ work/blog.py
@@ -14,8 +14,8 @@ def file(root):
         text = open(path, encoding='utf-8').read()
         total = total + "\n" + text
 
-    rec = re.compile("[^\u4E00-\u9FA5]")
-    total = rec.sub("", total)
+    # rec = re.compile("[^\u4E00-\u9FA5]")
+    # total = rec.sub("", total)
     wordlist = jieba.cut(total, cut_all=True)
     wl = " ".join(wordlist)
     # wl = "1111 qqq 中国"
@@ -28,10 +28,10 @@ def file(root):
         max_words=1000,
         # 这种字体都在电脑字体中，window在C:\Windows\Fonts\下，mac我选的是/System/Library/Fonts/PingFang.ttc 字体
         font_path='C:\\Windows\\Fonts\\STFANGSO.ttf',
-        height=2000,
-        width=2000,
+        height=4000,
+        width=4000,
         # 设置字体最大值
-        max_font_size=200,
+        max_font_size=250,
         # 设置有多少种随机生成状态，即有多少种配色方案
         random_state=30,
     )
