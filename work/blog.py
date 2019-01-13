2196b95f44a8d2d3c7b944cc0d65d72d9eea7e03
tx <467220125@qq.com>
2019-01-13 12:40:54 +0800

    Fix

--- work/blog.py
+++ work/blog.py
@@ -9,9 +9,10 @@ def file(root):
     total = ""
     file_list = os.listdir(root)
     for file in file_list:
-        path = os.path.join(root,file)
-        text = open(path, encoding='utf-8').read()
-        total = total + "\n" + text
+        if os.path.splitext(file)[1] == ".md":
+            path = os.path.join(root,file)
+            text = open(path, encoding='utf-8').read()
+            total = total + "\n" + text
 
     # rec = re.compile("[^\u4E00-\u9FA5]")
     # total = rec.sub("", total)
@@ -26,9 +27,9 @@ def file(root):
         # 设置最大显示的词云数
         max_words=1000,
         # 这种字体都在电脑字体中，window在C:\Windows\Fonts\下，mac下的是/System/Library/Fonts/PingFang.ttc 字体
-        font_path='C:\\Windows\\Fonts\\STFANGSO.ttf',
-        height=4000,
-        width=4000,
+        font_path='/System/Library/Fonts/PingFang.ttc',
+        height=2000,
+        width=2000,
         # 设置字体最大值
         max_font_size=250,
         # 设置有多少种随机生成状态，即有多少种配色方案
@@ -38,16 +39,19 @@ def file(root):
     # 展示词云图
     plt.imshow(myword)
     plt.axis("off")
-    plt.savefig("getsex.png")  # 保存图片
-    plt.ion()
-    plt.pause(5)
-    plt.close()  # 图片显示5s，之后关闭
-    # plt.show()
+    wc.to_file('blog.png')  # 把词云保存下
+    # plt.savefig("blog.png")  #
+    # plt.ion()
+    # plt.pause(5)
+    # plt.close()  # 图片显示5s，之后关闭
+    plt.show()
     # wc.to_file('blog.png')  # 把词云保存下
+
+
 
 def word(filename):
     text = open(filename, encoding='utf-8').read()
     print(text)
 
 # word("F:\新建文件夹\md\关于我.md")
-file("F:\新建文件夹\md")
+file("/Users/tx/mine/资源/md")
